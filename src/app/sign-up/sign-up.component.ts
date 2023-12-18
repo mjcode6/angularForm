3a37bd0a074d50520be7f8c666f4f3d9b3ec025a
sivasuthan manojan <mjcode6@gmail.com>
2023-12-18 14:56:32 +0100

    terminé

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/*import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+} */
+
+import { Component } from '@angular/core';
+import { User } from './user.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+  user: User = new User('', '', '', '');
+
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      firstName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(15),
+        ],
+      ],
+      lastName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+  onSubmit() {
+    if (this.signupForm.valid) {
+      console.log('User submitted:', this.signupForm.value);
+    } else {
+      console.error('Form is invalid. Please check the fields.');
+    }
+  }
 }
